3bc39dadb644fe8e155f0a3be15ce17af24e2e0c
장주현 <joohyun852@gmail.com>
2025-01-10 15:55:30 +0900

    code update

--- scripts/data_download.sh
+++ scripts/data_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python run.py \
     --task_name data_download \
-    --output_dir ./stock_data/Nasdaq30/ \
+    --output_dir ./datasets/Nasdaq/ \
     --tickers AAPL NVDA MSFT GOOG AMZN \
     --start_day 2010-06-01 \
     --end_day 2025-01-01 \
